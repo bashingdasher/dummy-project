2a7ef730eb74b7814191765212d9e746c0baf1cd
apacchi <87180120+apacchi@users.noreply.github.com>
2023-07-25 13:01:41 +0330

    added task model to todolist, created app for login/register

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-updated"]
+
+    def __str__(self):
+        return "{} - {} - {}".format(self.author, self.title, self.updated)
+
