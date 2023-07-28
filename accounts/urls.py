ec9dbe5ca50e7d80e92d93d8268cb492fcb12681
h3sam <abhaji.hesam@gmail.com>
2023-07-28 13:21:48 +0330

    added logout page functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-
+app_name = "accounts"
 urlpatterns = [
     path("register/", views.registerUser, name="register-user"),
     #TODO: make a page for displaying user info and apps they can access
-    # path("", views.userInfo, name="user-info"),
+    # path("<str:pk>/", views.userInfo, name="user-info"),
     path("login/", views.loginUser, name="login-user"),
+    path("logout/", views.userLogout, name="logout-user")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from .forms import RegisterForm
 from base.views import indexPage
 
@@ -34,15 +34,20 @@ def loginUser(request):
         return HttpResponse("you are already logged in!")
 
     if request.method == "POST":
-        try:
-            user = User.objects.get(username=request.POST.get("username"))
-        except:
-            return HttpResponse("user does not exist")
-        else:
+            user = get_object_or_404(User, username = request.POST.get('username'))
             if user.check_password(request.POST.get("password")):
                 login(request, user)
                 redirect("/")
             else:
                 return HttpResponse("password is wrong. try logging in  again")
     return render(request, "accounts/loginpage.html")
+
+def userLogout(request):
+     if request.method == "POST":
+          next = request.POST.get("next")
+          logout(request)
+          return redirect(next)
+
+
+     return render(request, "accounts/logout-confirm.html")
 
