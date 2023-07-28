8af66dddc21d7f6f9fd1ac69c191b3417553ab62
h3sam <abhaji.hesam@gmail.com>
2023-07-28 12:16:45 +0330

    added main template functionality, and wired basic urls

--- base/views.py
+++ base/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def indexPage(request):
-    return HttpResponse("this is a test")
+    return render(request, "base/index.html")
